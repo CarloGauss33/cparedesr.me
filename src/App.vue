c05e44466e48f3ffbf75eba258e372924ca02724
Carlos Paredes <cparedesr@uc.cl>
2023-02-04 00:54:14 -0300

    refactor(): migrate to vue3 + tailwind + vite

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+
+<template>
+    <div class="bg-gradient-to-r from-blue-500 to-purple-500 animate-gradient-x">
+        <div class="flex flex-col min-h-screen h-screen">
+            <div class="container mx-auto pt py-4 md:py-16 px-4 md:px-16">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/home.vue
@@ -0,0 +1,98 @@
+<script setup lang="ts">
+import emailIconUrl from '../assets/email.svg'
+import linkedinIconUrl from '../assets/linkedin.svg'
+import telegramIconUrl from '../assets/telegram.svg'
+import githubIconUrl from '../assets/github.svg'
+import twitterIconUrl from '../assets/twitter.svg'
+import mePhotoUrl from '../assets/me.jpg'
+
+
+
+const name = 'Carlos Paredes Robles'
+
+const contact = {
+  email: {
+    name: 'carlos@paredesrobles.com',
+    url: 'mailto:carlos@paredesrobles.com'
+  },
+  telegram: {
+    name: '@cparedesr',
+    url: 'https://t.me/cparedesr'
+  },
+  github: {
+    name: 'CarloGauss33',
+    url: 'https://github.com/CarloGauss33'
+  },
+  linkedin: {
+    name: 'Carlos Paredes',
+    url: 'https://www.linkedin.com/in/cparedesr-cl'
+  },
+  twitter: {
+    name: '@CarloGauss33',
+    url: 'https://twitter.com/CarloGauss33'
+  }
+}
+</script>
+
+<template>
+  <div class="w-full rounded-lg bg-white px-4 py-6 flex flex-col items-center shadow-lg">
+    <h1 class="text-3xl font-bold text-gray-800 md:mb-2 text-center">{{ name }}</h1>
+    <h3
+      class="text-gray-800 text-xl text-center mb-4 md:mb-8">
+      Cypherpunk and Bitcoiner
+    </h3>
+
+    <div
+      class="flex flex-col md:flex-row w-full md:px-24 items-center md:items-start mb-4 md:mb-16">
+
+      <img :src="mePhotoUrl" class="w-36 h-36 mb-4 md:mb-0 md:w-48 md:h-48 rounded-xl shadow-lg md:mr-12" />
+      <div class="flex flex-col gap-2">
+        <p class="text-gray-800 text-justify md:text-left">
+          I'm a software engineer from Chile, Bitcoin Maximalist and Cypherpunk.
+          I'm working at <a href="https://platan.us" class="text-yellow-400 drop-shadow-xl font-bold hover:text-purple-500">Platan.us</a>.
+        </p>
+        <p class="text-gray-800 text-justify md:text-left">
+          Also I'm a student at Pontificia Universidad Católica de Chile where I'm currently leading the team for the course
+          IIC2133 - Data Structures and Algorithms.
+        </p>
+        <div class="p-2 flex">
+          <a href="/selfhosted" class="bg-yellow-400 mx-auto text-white px-4 py-2 rounded-lg shadow-lg hover:bg-purple-500">
+              Some of my selfhosted projects
+          </a>
+        </div>
+        <p class="text-gray-800 text-justify text-sm md:text-left">
+          Key interests: Bitcoin, Algorithms, Privacy, Selfhosting, Startups, International Relationships, and more.
+        </p>
+      </div>
+    </div>
+
+
+    <div class="grid grid-cols-2 md:grid-cols-5 md:px-12 gap-8 md:gap-8">
+      <a :href="contact.github.url" class="transition ease-in-out delay-150 flex flex-row gap-1 items-center hover:-translate-y-1 hover:scale-110 duration-150">
+        <img :src="githubIconUrl" class="w-8 h-8" />
+        <span class="text-gray-800 text-sm">{{ contact.github.name }}</span>
+      </a>
+
+      <a :href="contact.linkedin.url" class="transition ease-in-out delay-150 flex flex-row gap-1 items-center hover:-translate-y-1 hover:scale-110 duration-150">
+        <img :src="linkedinIconUrl" class="w-8 h-8" />
+        <span class="text-gray-800 text-sm">{{ contact.linkedin.name }}</span>
+      </a>
+
+      <a :href="contact.telegram.url" class="transition ease-in-out delay-150 flex flex-row gap-1 items-center hover:-translate-y-1 hover:scale-110 duration-150">
+        <img :src="telegramIconUrl" class="w-8 h-8" />
+        <span class="text-gray-800 text-sm">{{ contact.telegram.name }}</span>
+      </a>
+
+
+      <a :href="contact.twitter.url" class="transition ease-in-out delay-150 flex flex-row gap-1 items-center hover:-translate-y-1 hover:scale-110 duration-150">
+        <img :src="twitterIconUrl" class="w-8 h-8" />
+        <span class="text-gray-800 text-sm">{{ contact.twitter.name }}</span>
+      </a>
+
+      <a :href="contact.email.url" class="md:col-span-1 col-span-2 transition ease-in-out delay-150 flex flex-row gap-1 items-center hover:-translate-y-1 hover:scale-110 duration-150">
+        <img :src="emailIconUrl" class="w-8 h-8" />
+        <span class="text-gray-800 text-sm">{{ contact.email.name }}</span>
+      </a>
+    </div>
+  </div>
+</template>
