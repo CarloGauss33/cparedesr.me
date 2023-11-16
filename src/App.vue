c9314088bef4f2fa9f20d5db560e9a09c5cd468a
Carlos Paredes <carlos@paredesrobles.com>
2023-11-15 23:36:51 -0300

    feat(Work): update current job

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import { ref } from 'vue'
 </script>
 
 <template>
-    <div class="bg-gradient-to-r from-blue-500 to-purple-500 animate-gradient-x">
-        <div class="flex flex-col min-h-screen h-screen">
+    <div class="bg-gradient-to-r from-blue-500 min-h-screen to-purple-500 animate-gradient-x">
+        <div class="flex flex-col">
             <div class="container mx-auto pt py-4 md:py-16 px-4 md:px-16">
                 <router-view></router-view>
             </div>
--- src/components/home.vue
+++ src/components/home.vue
@@ -36,23 +36,18 @@ const contact = {
 
 <template>
   <div class="w-full rounded-lg bg-white px-4 py-6 flex flex-col items-center shadow-lg">
-    <h1 class="text-3xl font-bold text-gray-800 md:mb-2 text-center">{{ name }}</h1>
-    <h3
-      class="text-gray-800 text-xl text-center mb-4 md:mb-8">
-      Cypherpunk and Bitcoiner
-    </h3>
-
+    <h1 class="text-3xl font-bold text-gray-800 md:mb-8 text-center">{{ name }}</h1>
     <div
       class="flex flex-col md:flex-row w-full md:px-24 items-center md:items-start mb-4 md:mb-16">
 
       <img :src="mePhotoUrl" class="w-36 h-36 mb-4 md:mb-0 md:w-48 md:h-48 rounded-xl shadow-lg md:mr-12" />
       <div class="flex flex-col gap-2">
         <p class="text-gray-800 text-justify md:text-left">
-          I'm a software engineer from Chile, Bitcoin Maximalist and Cypherpunk.
-          I'm working at <a href="https://platan.us" class="text-yellow-400 drop-shadow-xl font-bold hover:text-purple-500">Platan.us</a>.
+          I'm a engineer from Chile, Bitcoin Maximalist and Cypherpunk.
+          Currently working at <a href="https://www.grupomok.com/" class="font-bold text-rose-700">MOK Labs</a> as a Senior Software Engineer.
         </p>
         <p class="text-gray-800 text-justify md:text-left">
-          Also I'm a student at Pontificia Universidad Católica de Chile where I'm currently leading the team for the course
+          Additionally, I am a student at the Pontificia Universidad Católica de Chile, where I currently lead the team for the course
           IIC2133 - Data Structures and Algorithms.
         </p>
         <div class="p-2 flex flex-col justify-start items-start w-full gap-2">
@@ -63,8 +58,8 @@ const contact = {
           <a href="https://pdf.cparedesr.com" class="bg-purple-400 w-full text-white px-4 py-2 rounded-lg shadow-lg hover:bg-yellow-500">
               CPDF - PDF editor
           </a>
-          <a href="https://draw.cparedesr.com/" class="bg-purple-400 w-full text-white px-4 py-2 rounded-lg shadow-lg hover:bg-yellow-500">
-              Draw - Excalidraw without tracking
+          <a href="https://blog.cparedesr.com/" class="bg-purple-400 w-full text-white px-4 py-2 rounded-lg shadow-lg hover:bg-yellow-500">
+            Blog - Personal blog
           </a>
         </div>
         <p class="text-gray-800 text-justify text-sm md:text-left">
